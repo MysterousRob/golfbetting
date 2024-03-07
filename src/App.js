df03c1674378c9565f17accae6d7bf6aa07b5cd9
MysteriousRob <robertjohngasior@gmail.com>
2024-03-07 14:48:37 +0100

    added html/css in index

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className='in-body'>
-      <header className='to-head'>
-          <h1 className='header'>Golf Betting</h1>
+    <header className="header">
+  <div className="head" className="header">
+    <h1>Golf Betting</h1>
+        <div className="nav">
           <div className='buttons'>
-            <button href=""></button>
-            <button href=""></button>
+            <button href="">Betting tips</button>
+            <button href="">Live Odds</button>
             <button href=""></button>
           </div>
       </header>
+      <section>
+
+      </section>
     </div>
   );
 }
