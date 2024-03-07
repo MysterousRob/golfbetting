dc9106307035ae2151e4dee229cc4b807279ab4e
MysteriousRob <robertjohngasior@gmail.com>
2024-03-07 14:24:21 +0100

    made tournaments.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 
+
+
+//function NewPage
+
+
 function App() {
   return (
     <div className='in-body'>
       <header className='to-head'>
           <h1 className='header'>Golf Betting</h1>
           <div className='buttons'>
-
+            <button href=""></button>
+            <button href=""></button>
+            <button href=""></button>
           </div>
       </header>
     </div>
---
+++ src/tournaments.js
@@ -0,0 +1,37 @@
+
+
+function Competition(){
+    const theMasters{
+        return (
+            <div>
+            <h1>The Masters</h1>
+            <p>The Masters Tournament at Augusta National Golf Club is a prestigious event in golf, showcasing top players and iconic moments, while also embracing the excitement of betting for fans worldwide.</p>
+            </div>
+        );
+    };
+    const usOpen{
+        return (
+            <div>
+            <h1>The Us Open</h1>
+            <p>The US Open, one of golf's major championships, is a highly anticipated event that tests the skills and endurance of top players on challenging courses, captivating fans with its rich history and intense competition.</p>
+            </div>
+        );
+    };
+    const theOpen{
+        return (
+            <div>
+            <h1>The Open Championship</h1>
+            <p>The Open Championship, often referred to as the "British Open," is one of golf's oldest and most prestigious tournaments, steeped in tradition and held on iconic links courses in the United Kingdom.</p>
+            </div>
+        );
+    };
+    const pgaUs {
+        return (
+            <div>
+            <h1>US PGA Championship</h1>
+            <p>The US PGA Championship, one of golf's major tournaments, showcases the world's top players battling for supremacy on diverse courses across the United States, delivering thrilling moments and intense competition.</p>
+            </div>
+        );
+    };
+}
+export default Competition;
