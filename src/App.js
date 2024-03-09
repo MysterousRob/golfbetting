1fa503e6b5a84cb7f6558b63d1a4fcc2b01ae9e0
MysteriousRob <robertjohngasior@gmail.com>
2024-03-09 13:59:06 +0100

    buttons not working yet perpactly

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import Competiton from './tournaments/tournaments';
 
 //function NewPage
 
-
 function App() {
   return (
     <div className='in-body'>
       <header className='App-header'>
         <h1>Golf Betting</h1>
+      <div className='buttons'>
+        <button onClick= {() => console.log ('Betting Tips button clicked')}><span>Betting Tips</span></button> 
+        <button onClick= {() => console.log ('Live Odds button clicked')}><span>Live Odds</span></button>
+        <button onClick= {() => console.log ('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
+      </div>  
       </header>
-        <button>Betting Tips</button> 
-        <button>Live Odds</button>
-        <button>Bets of the Week</button>
+      <div>
+        <Competiton />
+      </div>
+      
     </div> 
+      
+      
   );
 }
 
