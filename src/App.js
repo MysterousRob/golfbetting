0cc4100763c85dfa161f3430d8d092a326935e0a
MysteriousRob <robertjohngasior@gmail.com>
2024-03-09 17:45:20 +0100

    added home

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className='App-header'>
         <h1 className='h1'>Golf Betting</h1>
       <div className='nav'>
+        <button href="#" onClick= {() => console.log('Home button clicked')}><span>Home</span></button>
         <button href="#" onClick= {() => console.log('Betting Tips button clicked')}><span>Betting Tips</span></button> 
         <button href="#" onClick= {() => console.log('Live Odds button clicked')}><span>Live Odds</span></button>
         <button href="#" onClick= {() => console.log('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
