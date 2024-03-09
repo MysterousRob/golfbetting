de9b3c0d12f8f753d92813e0900c984dbb2ebbb6
MysteriousRob <robertjohngasior@gmail.com>
2024-03-09 14:47:59 +0100

    improoved buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM  from 'react';
 import './App.css';
 import './index.css';
 import Competiton from './tournaments/tournaments';
@@ -11,10 +10,10 @@ function App() {
     <div className='in-body'>
       <header className='App-header'>
         <h1>Golf Betting</h1>
-      <div className='buttons'>
-        <button onClick= {() => console.log ('Betting Tips button clicked')}><span>Betting Tips</span></button> 
-        <button onClick= {() => console.log ('Live Odds button clicked')}><span>Live Odds</span></button>
-        <button onClick= {() => console.log ('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
+      <div className='nav'>
+        <button href="#" onClick= {() => console.log('Betting Tips button clicked')}><span>Betting Tips</span></button> 
+        <button href="#" onClick= {() => console.log('Live Odds button clicked')}><span>Live Odds</span></button>
+        <button href="#" onClick= {() => console.log('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
       </div>  
       </header>
       <div>
