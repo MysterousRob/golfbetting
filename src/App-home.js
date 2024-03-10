ae01a8c06880d581af146a68f52be99b442fc148
MysteriousRob <robertjohngasior@gmail.com>
2024-03-10 20:41:13 +0100

    addedonclick window location

--- src/App-home.js
+++ src/App-home.js
@@ -10,15 +10,35 @@ function App() {
       <header className='App-header'>
         <h1 className='h1'>Golf Betting</h1>
       <div className='nav'>
-        <button href="./C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\App-home.js-home" onClick= {() => console.log('Home button clicked')}><span>Home</span></button>
-        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\golfbettingtips.js" onClick= {() => console.log('Betting Tips button clicked')}><span>Betting Tips</span></button> 
-        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\livegolfodds.js" onClick= {() => console.log('Live Odds button clicked')}><span>Live Odds</span></button>
-        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\Betsoftheweek.js" onClick= {() => console.log('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
-        <button href="#" onClick= {() => console.log('Betting Sites button clicked')}><span>Betting Sites</span></button>
+        <button onClick= {() => {window.location.href = './App-home.js-home'}}>
+          <span>
+            Home
+          </span>
+        </button>
+        <button onClick= {() =>  {window.location.href = './golfbettingtips.js'}}>
+          <span>
+            Betting Tips
+          </span>
+        </button> 
+        <button onClick= {() =>  {window.location.href = './golfbettingtips.js'}}>
+          <span>
+            Live Odds
+          </span>
+        </button>
+        <button onClick= {() =>   {window.location.href = './golfbettingtips.js'}}>
+          <span>
+            Bets of the Week
+          </span>
+        </button>
+        <button href="#" onClick= {() => console.log('Betting Sites button clicked')}>
+          <span>
+            Betting Sites
+          </span>
+        </button>
       </div>  
       </header>
       <div className='tips'>
-        <h1>Different Golf Betting Websites</h1>
+        <h1>Home</h1>
       </div>
     </div> 
   );
---
+++ src/golfbettingsites.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Competiton from './tournaments/tournaments';
+//function NewPage
+
+function App() {
+  return (
+    <div className='in-body'>
+      <header className='App-header'>
+        <h1 className='h1'>Golf Betting</h1>
+      <div className='nav'>
+        <button href="./C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\App-home.js-home" onClick= {() => console.log('Home button clicked')}><span>Home</span></button>
+        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\golfbettingtips.js" onClick= {() => console.log('Betting Tips button clicked')}><span>Betting Tips</span></button> 
+        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\livegolfodds.js" onClick= {() => console.log('Live Odds button clicked')}><span>Live Odds</span></button>
+        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\Betsoftheweek.js" onClick= {() => console.log('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
+        <button href="C:\Users\rober\OneDrive\Desktop\Golf-betting\golfbetting\src\golfbettingsites.js" onClick= {() => console.log('Betting Sites button clicked')}><span>Betting Sites</span></button>
+      </div>  
+      </header>
+      <div className='tips'>
+        <h1>Different Golf Betting Websites</h1>
+      </div>
+    </div> 
+  );
+}
+
+
+export default App;
+
