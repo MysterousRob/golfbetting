1665541e14ce3c0bd7bc308b44b05dca78877776
MysteriousRob <robertjohngasior@gmail.com>
2024-03-10 19:13:37 +0100

    made files for my buttons

---
+++ src/Betsoftheweek.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Competiton from './tournaments/tournaments';
+//function NewPage
+
+function App() {
+  return (
+    <div className='in-body'>
+      <header className='App-header'>
+        <h1 className='h1'>Golf Betting</h1>
+      <div className='nav'>
+        <button href="#" onClick= {() => console.log('Home button clicked')}><span>Home</span></button>
+        <button href="#" onClick= {() => console.log('Betting Tips button clicked')}><span>Betting Tips</span></button> 
+        <button href="#" onClick= {() => console.log('Live Odds button clicked')}><span>Live Odds</span></button>
+        <button href="#" onClick= {() => console.log('Bets of the Week button clicked')}><span>Bets of the Week</span></button>
+      </div>  
+      </header>
+      <div className='tips'>
+        <h1>Golf Bets of the week</h1>
+      </div>
+    </div> 
+  );
+}
+
+
+export default App;
+
