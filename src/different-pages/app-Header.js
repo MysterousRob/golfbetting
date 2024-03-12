25a67dc58e1fdf1af228a215eb733d17d23d5c78
MysteriousRob <robertjohngasior@gmail.com>
2024-03-12 11:17:43 +0100

    added header into separate file for later recall

---
+++ src/different-pages/app-Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import Competiton from './tournaments/tournaments';
+//function NewPage
+
+
+
+function Header() {
+  
+  return (
+    <div className='in-body'>
+      <header className='App-header'>
+        <h1 className='h1'>
+          Golf Betting
+        </h1>
+      <div className='nav'>
+        {/*need to figgure out why my nav buttons arent working as i want them to nav to individual files */}
+        <button onClick= {}>
+          <span>
+            Home
+          </span>
+        </button>
+        <button onClick= {() => {window.location.href = './golfbettingtips.js-betingtips'}}>
+          <span>
+            Betting Tips
+          </span>
+        </button>
+        <button onClick= {() => {window.location.href = './livegolfodds.js-LiveOdds'}}>
+          <span>
+            Live Odds
+          </span>
+        </button>
+        <button onClick= {() => {window.location.href = './Betsoftheweek.jsBetsoftheweek'}}>
+          <span>
+            Bets of the Week
+          </span>
+        </button>
+        <button onClick= {() => {window.location.href = './golfbettingsites.js-bettingsites'}}>
+          <span>
+            Betting Sites
+          </span>
+        </button>
+      </div>  
+      </header>
+      <div className='tips'>
+        <h1>
+          Home
+        </h1>
+      </div>
+    </div> 
+  );
+
+}
+
+
+export default Header;
+
--- src/nav/nav.js
+++ src/nav/nav.js
@@ -22,4 +22,40 @@ export const navOptions = [
       href: "different-pages\livegolfodds.js",
       title: "Live Odds",
     },
+    
+]
+
+export const tournaments [
+  {
+    href: "different-pages\tournaments\openchamp.js",
+    title: "The Open",
+  },
+  {
+    href: "different-pages\tournaments\themaster.js",
+    title: "The Masters",
+  },
+  {
+    href: "different-pages\tournaments\theopen.js",
+    title: "The British Open",
+  },,
+  {
+    href: "different-pages\tournaments\uspga.js",
+    title: "US PGA Championship",
+  },
+  {
+    href: "different-pages\tournaments\tournaments.js",
+    title: "Tournaments",
+  },
+  {
+    href: "",
+    title: "",
+  },
+  {
+    href: "",
+    title: "",
+  },
+  {
+    href: "",
+    title: "",
+  },
 ]
