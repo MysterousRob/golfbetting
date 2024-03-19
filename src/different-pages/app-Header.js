8853d35ad5b3bd325d324f2392d48575245365f0
MysteriousRob <robertjohngasior@gmail.com>
2024-03-19 13:40:10 +0100

    started to work on betting tips page

--- src/different-pages/app-Header.js
+++ src/different-pages/app-Header.js
@@ -13,35 +13,7 @@ function Header() {
         </h1>
       <div className='nav'>
         {/*need to figgure out why my nav buttons arent working as i want them to nav to individual files */}
-        {/* <Link to={navOptions[0].href}>
-           <span>
-            Home
-           </span>
-        </Link>
-
-        <Link to={navOptions[1].href}>
-          <span>
-            Betting Tips
-          </span>
-        </Link>
-
-        <Link to={navOptions[2].href}>
-          <span>
-            Live Odds
-          </span>
-        </Link>
-
-        <Link to={navOptions[3].href}>
-          <span>
-            Bets of the Week
-          </span>
-        </Link>
-
-        <Link to={navOptions[4].href}>
-          <span>
-            Betting Sites
-          </span>
-        </Link> */}
+      
         <Nav />
       </div>  
       </header>
