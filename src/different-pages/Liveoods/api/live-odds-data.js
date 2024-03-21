d06fe3d973099d1d7bc50f679277e6d8a0653054
MysteriousRob <robertjohngasior@gmail.com>
2024-03-21 22:07:37 +0100

    made footer and started to map need to make structure of footer

---
+++ src/different-pages/Liveoods/api/live-odds-data.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import '../../forhomepg/footer-css/footer.css';
+
+//making nested array (FOLLOW STRUCTURE, DONT DOUBT IT, JUST DO IT)
+export const foot = [
+    {
+        title: "",
+        links:  [
+            {
+                title: "",
+                to: "",
+            }
+        ],
+    },
+]
