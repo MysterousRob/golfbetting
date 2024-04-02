31a118cc692b7e315880cf8878742aff827ca9f2
MysteriousRob <robertjohngasior@gmail.com>
2024-04-02 13:04:52 +0200

    finished my array for slider data, now need to map it out

--- src/different-pages/Liveoods/slider components/sliderdata.js
+++ src/different-pages/Liveoods/slider components/sliderdata.js
@@ -20,19 +20,125 @@ const sliderData = [
         ]
     },
     {
-        name: ,
+        form: "Current Form",
         name2: [
             {
-
-            }
+                name: "OWGR",
+            },
+            {
+                name: "Last Tournament",
+            },
+            {
+                name: "Last but 1 Tournament",
+            },
+            {
+                name: "Last but 3 Tournament",
+            },
+            {
+                name: "Last but 4 Tournament",
+            },
         ]
-    }
+    },
     {
-        name: ,
+        form: "scoring stats",
         name2: [
             {
-
-            }
+                name: "Hole in One Average",
+            },
+            {
+                name: "Birdie Average",
+            },
+            {
+                name: "Eagle Average",
+            },
+            {
+                name: "Bogey Average",
+            },
+            {
+                name: "Par 3 Performance",
+            },
+            {
+                name: "Par 4 Performance",
+            },
+            {
+                name: "Par 5 Performance",
+            },
         ]
-    }
+    },
+    {
+        form: "Event Spacific",
+        name2: [
+            {
+                name: "Par 70",
+            },
+            {
+                name: "Short Course",
+            },
+            {
+                name: "Bermuda Postive",
+            },
+            {
+                name: "Soft Ground",
+            },
+            {
+                name: "Fazio Design",
+            },
+            {
+                name: "Costal Course",
+            },
+        ]
+    },
+    {
+        form: "Strokes Gained",
+        name2: [
+            {
+                name: "SG off the Tee",
+            },
+            {
+                name: "SG Approach",
+            },
+            {
+                name: "SG Tee to Green",
+            },
+            {
+                name: "SG arround the Green",
+            },
+            {
+                name: "SG Putting",
+            },
+        ]
+    },
+    {
+        form: "Skill Stats",
+        name2: [
+            {
+                name: "Driving Distance",
+            },
+            {
+                name: "Proximity to Hole",
+            },
+            {
+                name: "Going for Green",
+            },
+            {
+                name: "Driving Acuracy",
+            },
+            {
+                name: "GIR",
+            },
+            {
+                name: "Scrambling",
+            },
+            {
+                name: "Sand Saves",
+            },
+            {
+                name: "Putting Average",
+            },
+            {
+                name: "Puts per Round",
+            },
+        ]
+    },
+   
 ]
