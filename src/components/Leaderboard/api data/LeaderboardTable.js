fefd747e6570a788fa80883d87903d8f6c8d3b0a
MysteriousRob <robertjohngasior@gmail.com>
2024-05-02 15:05:20 +0200

    ci added more styling to leaderboard and added heroicons

--- src/components/Leaderboard/api data/LeaderboardTable.js
+++ src/components/Leaderboard/api data/LeaderboardTable.js
@@ -1,37 +1,63 @@
 import React from "react";
-import '../../../assets/css/GBS.css'
+import '../../../assets/css/GBS.css';
+//image icon imports
+import { MdLeaderboard } from "react-icons/md";
+import { GrScorecard } from "react-icons/gr";
+import { TbSquareRoundedPercentage } from "react-icons/tb";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+
+
 
 function LeaderboardOutline(){
     return(
-        <div className="leaderboard">
-      <h2>Leaderboard:</h2>
-      <table className="container2">
-        <thead className="container3">
-          <tr>
-            <th>Rank</th>
-            <th>Player Name</th>
-            <th>Score</th>
-            <th>Odds</th>
-          </tr>
-        </thead>
-        <tbody className="container4">
-        <tr>
-              <td>index</td>
-              <td>player name</td>
-              <td>player score</td>
-              <td>Odds</td>
-            </tr>
-          {/* {players.map((player, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{player.name}</td>
-              <td>{player.score}</td>
-              <td>{player.odds}</td>
-            </tr>
-          ))} */}
-        </tbody>
-      </table>
-    </div>
+        <div className="NEO">
+            <h2>Leaderboard:</h2>
+            <div className="leaderboard">
+                  
+                <table className="container2">
+            <thead className="container3">
+              <tr className="container-box">
+              <th className="text-th">
+                Rank <MdLeaderboard className="icon" />
+              </th>
+              <th className="text-th">
+                Player Name <MdDriveFileRenameOutline className="icon" />
+              </th>
+              <th className="text-th">
+                Score <GrScorecard className="icon" />
+              </th>
+              <th className="text-th">
+                Odds <TbSquareRoundedPercentage className="icon" />
+              </th>
+              </tr>
+            </thead>
+            <tbody className="container4">
+            <tr className="container-box">
+                <td className="text-tb">
+                    index
+                </td>
+                <td className="text-tb">
+                    player name
+                </td>
+                <td className="text-tb">
+                    player score
+                </td>
+                <td className="text-tb">
+                    Odds
+                </td>
+                </tr>
+              {/* {players.map((player, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{player.name}</td>
+                  <td>{player.score}</td>
+                  <td>{player.odds}</td>
+                </tr>
+              ))} */}
+            </tbody>
+                  </table>
+                </div>
+        </div>
     )
 }
 
